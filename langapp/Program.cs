4b54a2b37c3c852728a219419c590a57d9a4f439
Stuart Hodgins <stuart.hodgins@uwaterloo.ca>
2016-09-18 20:33:39 -0400

    Added Unicode encoding. Also added export of EN_US file for testing.

--- langapp/Program.cs
+++ langapp/Program.cs
@@ -12,15 +12,17 @@ namespace ConsoleApplication
                 Console.WriteLine("Usage: Single argument (required) is base of CSV file name.");
                 Environment.Exit(0);      
             }
-            Console.WriteLine("Transforming CSV file " + args[0] + " to fr_CA.lang.");
+            Console.WriteLine("Transforming CSV file {0} to fr_CA.lang.", args[0]);
 
             try {
                 string[] lines = System.IO.File.ReadAllLines(args[0] + ".csv");
 
-                StringBuilder builder = new StringBuilder();
+                StringBuilder builderEN = new StringBuilder();
+                StringBuilder builderFR = new StringBuilder();
 
                 // Start with open parenthesis
-                builder.AppendLine("{");
+                builderEN.AppendLine("{");
+                builderFR.AppendLine("{");
 
                 foreach (string line in lines)
                 {
@@ -41,22 +43,33 @@ namespace ConsoleApplication
                         else {
                             // Export the key and the french text to the CSV file.
                             // Console.WriteLine("Dumping line for "+temp[0].Trim()+);
-                            builder.AppendLine("    \""+temp[0].Trim()+"\" , \""+temp[2].Trim()+"\",");
+                            builderEN.AppendLine("    \""+temp[0].Trim()+"\" : \""+temp[1].Trim()+"\",");
+                            builderFR.AppendLine("    \""+temp[0].Trim()+"\" : \""+temp[2].Trim()+"\",");
                         }
                     }
                 }
-                // End  with close parenthesis
-                builder.AppendLine("}");
+                // End with close parenthesis
+                builderEN.AppendLine("}");
+                builderFR.AppendLine("}");
 
-                // Dump all collected lines to the LANG file. 
+                // Dump all collected lines to the LANG files. 
                 try {
-                    System.IO.File.WriteAllText("fr_CA.lang", builder.ToString());
+                    // Lang 1 (English)
+                    System.IO.File.WriteAllText("en_US1.lang", builderEN.ToString(), Encoding.Unicode);
                 } catch (IOException) {
                     // Probably file in use. close it!
-                    Console.WriteLine("Hey! Something went wrong. If your editor has the file fr_CA.lang open, please close it.");
+                    Console.WriteLine("Hey! Something went wrong. If your editor has the file en_US1.lang open, please close it.");
                 }
-            } catch (FileNotFoundException) {
-                Console.WriteLine("Hey! Something went wrong. Is "+args[0] + ".csv present in this directory?");   
+                try {
+                    // Lang 1 (Cdn French)
+                    System.IO.File.WriteAllText("fr_CA1.lang", builderFR.ToString(), Encoding.Unicode);
+                } catch (IOException) {
+                    // Probably file in use. close it!
+                    Console.WriteLine("Hey! Something went wrong. If your editor has the file fr_CA1.lang open, please close it.");
+                }
+            } 
+            catch (FileNotFoundException) {
+                Console.WriteLine("Hey! Something went wrong. Is {0}.csv present in this directory?", args[0]);   
             } catch (IOException) {
                 Console.WriteLine("Hey! Something went wrong with file IO. I don't know what.");   
             }
